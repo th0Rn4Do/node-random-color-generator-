b9677bd55bbf76fe525cbaf70864c48aa0e71dc2
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-04-25 15:37:41 +0200

    Add 2 boxes with hard coded hex code

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('hello');
+function square() {
+  for (let i = 0; i < 3; i++) {
+    // Runs 3 times, with values of step 0 through 4.
+    console.log('###############################');
+  }
+  for (let i = 0; i < 1; i++) {
+    // Runs 1 time, with values of step 0 through 4.
+    console.log('#####                     #####');
+  }
+  for (let i = 0; i < 1; i++) {
+    // Runs 1 times, with values of step 0 through 4.
+    console.log('#####       #ff0000       #####');
+  }
+  for (let i = 0; i < 1; i++) {
+    // Runs 1 times, with values of step 0 through 4.
+    console.log('#####                     #####');
+  }
+  for (let i = 0; i < 3; i++) {
+    // Runs 3 times, with values of step 0 through 4.
+    console.log('###############################');
+  }
+
+  return;
+}
+square();
+console.log('\n');
+square();
+
+/* for (i=0 , i < 3) {
+  console.log('###############################');
+  i++;
+}
+
+console.log('
+  ###############################
+  ###############################
+  ###############################
+  #####                     #####
+  #####       #ff0000       #####
+  #####                     #####
+  ###############################
+  ###############################
+  ###############################
+  ');*/
