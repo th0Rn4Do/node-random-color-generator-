981fb0d0ea4c199aa24f8e7fd59d93caa7f0b86a
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-04-25 17:38:43 +0200

    Rename function for first box

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import randomColor from 'randomcolor';
 
 const generatedColor = randomColor();
 
-function square() {
+function coloredSquareInRandomColor() {
   for (let i = 0; i < 3; i++) {
     // Runs 3 times, with values of step 0 through 2.
     console.log(chalk.hex(generatedColor)('###############################'));
@@ -59,18 +59,20 @@ function squareSpecifiedByUser(x) {
 }
 
 let colorSpecifiedByUser = randomColor({
-  luminosity: 'light',
-  hue: 'blue',
+  luminosity: 'dark',
+  hue: 'green',
 });
 
 //this output logs a 31x9 box which is colored in any random color
 console.log('\n');
 console.log('box colored in a random color');
-square();
+coloredSquareInRandomColor();
 
 //this output logs a 31x9 box which is colored in different shade of the random color above
 console.log('\n');
-console.log('box colored in a different shade of the random color');
+console.log(
+  'box colored in a different shade of the color specifed by user input',
+);
 squareSpecifiedByUser(colorSpecifiedByUser);
 
 // console.log(chalk.hex('#DEADED').underline('Hello, world!'));
