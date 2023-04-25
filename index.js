b60f5035e8202e4729bd64ee6446aa6e3f54df2c
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-04-25 17:32:26 +0200

    Add color specifications to second box

--- index.js
+++ index.js
@@ -33,6 +33,36 @@ function square() {
   return;
 }
 
+function squareSpecifiedByUser(x) {
+  for (let i = 0; i < 3; i++) {
+    // Runs 3 times, with values of step 0 through 2.
+    console.log(chalk.hex(x)('###############################'));
+  }
+  for (let i = 0; i < 1; i++) {
+    // Runs 1 time, with value of step 0.
+    console.log(chalk.hex(x)('#####                     #####'));
+  }
+  for (let i = 0; i < 1; i++) {
+    // Runs 1 time, with values of step 0 through 2.
+    console.log(chalk.hex(x)(`#####       ${x}       #####`));
+  }
+  for (let i = 0; i < 1; i++) {
+    // Runs 1 time, with value of step 0.
+    console.log(chalk.hex(x)('#####                     #####'));
+  }
+  for (let i = 0; i < 3; i++) {
+    // Runs 3 times, with values of step 0 through 2.
+    console.log(chalk.hex(x)('###############################'));
+  }
+
+  return;
+}
+
+let colorSpecifiedByUser = randomColor({
+  luminosity: 'light',
+  hue: 'blue',
+});
+
 //this output logs a 31x9 box which is colored in any random color
 console.log('\n');
 console.log('box colored in a random color');
@@ -41,7 +71,7 @@ square();
 //this output logs a 31x9 box which is colored in different shade of the random color above
 console.log('\n');
 console.log('box colored in a different shade of the random color');
-square();
+squareSpecifiedByUser(colorSpecifiedByUser);
 
 // console.log(chalk.hex('#DEADED').underline('Hello, world!'));
 // colorhex(ff0000);
